8ed7953fb087b74887e0ef69301add34917f9964
santhosh17s <santhoshloud@gmail.com>
2018-06-01 15:21:37 +0530

    project folder stracture re-alined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { WeatherCardComponent } from './weather-card/weather-card.component';
-import { AddCardComponent } from './add-card/add-card.component';
-import { DetailsComponent } from './details/details.component';
+
 import { AddComponent } from './pages/add/add.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { WeatherService } from 'src/app/services/weather.service';
 import { UiService } from 'src/app/services/uiservice.service';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from 'src/app/pages/home/home.component';
+import { WeatherCardComponent } from 'src/app/UI/weather-card/weather-card.component';
+import { AddCardComponent } from 'src/app/UI/add-card/add-card.component';
+import { DetailsComponent } from 'src/app/pages/details/details.component';
 
 @NgModule({
   declarations: [
